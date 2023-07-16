ae37d22a02056ee98b76713692cc067201937e2c
Olliver Hughes <ohughes1207@gmail.com>
2023-07-16 13:23:05 +0100

    Added more get commands to API

--- pokedex_logic/PokedexService.py
+++ pokedex_logic/PokedexService.py
@@ -1,8 +1,16 @@
+from db_configs.models import Pokemon, Variant, Type
+
 class PokedexService:
     def __init__(self):
         pass
-    def getAllPokemon(self):
-        return self.pokemonRecords
+    def getAllPokemon(self, db):
+        return db.query(Pokemon).all()
+
+    def getAllVariants(self, db):
+        return db.query(Variant).all()
+
+    def getAllType(self, db):
+        return db.query(Type).all()
 
     def getPokemonByName(self, pokemon_name : str):
         for pokemon in self.pokemonRecords:
