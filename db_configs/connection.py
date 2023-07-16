154c504068b67fe8ee7ebf0e5bb4271ab3a7ac9b
ohughes1207 <114885709+ohughes1207@users.noreply.github.com>
2023-07-16 11:55:27 +0100

    Update connection.py

--- db_configs/connection.py
+++ db_configs/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DB_URL = "postgresql://pokedexdb_lsx3_user:FkkwMmVfD185SnzAIHD0Vr3wQFsd0w79@dpg-cimnjo5gkuvgvh9vpevg-a.frankfurt-postgres.render.com/pokedexdb_lsx3"
+SQLALCHEMY_DB_URL = ""
 
 engine = create_engine(SQLALCHEMY_DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
