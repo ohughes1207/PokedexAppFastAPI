e25b34f32b2a0ddf3c049eb3909dabd63a89d1c0
Olliver Hughes <ohughes1207@gmail.com>
2023-07-19 23:46:11 +0100

    removed empty line

--- pokedex_logic/PokedexService.py
+++ pokedex_logic/PokedexService.py
@@ -16,5 +16,4 @@ class PokedexService:
         for pokemon in self.pokemonRecords:
             if pokemon['base_name'].lower().__contains__(pokemon_name.lower()):
                 return pokemon
-
         return {'message': 'Pokemon not found!'}
