fc243cd88946127b3eddf35fbc82d9819081768f
Olliver Hughes <ohughes1207@gmail.com>
2023-07-27 00:34:05 +0100

    migrated service

--- pokedex_logic/PokedexService.py
+++ pokedex_logic/PokedexService.py
@@ -1,22 +1,23 @@
 from db_configs.models import Pokemon, Variant, Type
+from sqlalchemy.orm import joinedload
 
 class PokedexService:
     def __init__(self):
         pass
     def getAllPokemon(self, db):
-        return db.query(Pokemon).all()
-
+        pokemon_list = db.query(Pokemon).all()
+        
     def getAllVariants(self, db):
         return db.query(Variant).all()
 
     def getAllType(self, db):
         return db.query(Type).all()
 
-    def getPokemonByName(self, pokemon_name : str):
-        for pokemon in self.pokemonRecords:
-            if pokemon['base_name'].lower().__contains__(pokemon_name.lower()):
-                return pokemon
-        return {'message': 'Pokemon not found!'}
+    def getPokemonByName(self, pokemon_name : str, db):
+        if len(db.query(Pokemon).filter(Pokemon.base_name.ilike(f"%{pokemon_name}%")).all())==0:
+            return {'message': 'Pokemon not found!'}
+        else:
+            return db.query(Pokemon).filter(Pokemon.base_name.ilike(f"%{pokemon_name}%")).all()
 
 
     def getAllPokemonPaginated(self, page, db):
